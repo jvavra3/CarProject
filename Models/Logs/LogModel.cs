2d3603c2aa0acda2bd3fcc8fb2be6d6392fc94e3
Jan Vávra <janvav3@gmail.com>
2021-08-22 16:33:06 +0200

    change

---
+++ Models/Logs/LogModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarProject.Models.Logs
+{
+    public class LogModel
+    {
+        [Key]
+        public int LogId { get; set; }
+        
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        
+        [Display(Name = "Ip Address")]
+        public string IpAddress { get; set; }
+
+        public string Area { get; set; }
+        
+        [Display(Name = "Time Access")]
+        public string TimeAccess { get; set; }
+
+
+    }
+}
